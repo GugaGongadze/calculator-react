03761ea7cc739edf18f4a789a9d480a5449f6f02
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-03-15 18:36:41 +0400

    Added styling to buttons

--- src/App/index.js
+++ src/App/index.js
@@ -5,34 +5,36 @@ class App extends Component {
 	render() {
 		return (
 			<div className="iphone">
-				<div className="special-operators">
-					<button className="grey-butt">AC</button>
-					<button className="grey-butt">+/-</button>
-					<button className="grey-butt">%</button>
-				</div>
-				<div className="operators">
-					<button className="grey-butt">÷</button>
-					<button className="grey-butt">x</button>
-					<button className="grey-butt">-</button>
-					<button className="grey-butt">+</button>
-					<button className="grey-butt">=</button>
-				</div>
-				<div className="positive-numbers">
-					<button className="grey-butt">7</button>
-					<button className="grey-butt">8</button>
-					<button className="grey-butt">9</button>
-					<button className="grey-butt">4</button>
-					<button className="grey-butt">5</button>
-					<button className="grey-butt">6</button>
-					<button className="grey-butt">1</button>
-					<button className="grey-butt">2</button>
-					<button className="grey-butt">3</button>
-				</div>
-				<div className="zero">
-					<button className="slanted-button">0</button>
-				</div>
-				<div className="point">
-					<button className="grey-butt">.</button>
+				<div className="controls">
+					<div className="special-operators">
+						<button className="special-button">AC</button>
+						<button className="special-button">+/-</button>
+						<button className="special-button">%</button>
+					</div>
+					<div className="operators">
+						<button className="operator-button">÷</button>
+						<button className="operator-button">x</button>
+						<button className="operator-button">-</button>
+						<button className="operator-button">+</button>
+						<button className="operator-button">=</button>
+					</div>
+					<div className="positive-numbers">
+						<button className="num-button">7</button>
+						<button className="num-button">8</button>
+						<button className="num-button">9</button>
+						<button className="num-button">4</button>
+						<button className="num-button">5</button>
+						<button className="num-button">6</button>
+						<button className="num-button">1</button>
+						<button className="num-button">2</button>
+						<button className="num-button">3</button>
+					</div>
+					<div className="zero">
+						<button className="slanted-button">0</button>
+					</div>
+					<div className="point">
+						<button className="num-button">.</button>
+					</div>
 				</div>
 			</div>
 		);
